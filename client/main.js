bff01d5ce4f757f0483c0e77966f8b7b0819cf79
Morgante Pell <morgante.pell@morgante.net>
2013-06-15 14:41:05 -0400

    after much pain, just moved bitly to client side

--- client/main.js
+++ client/main.js
@@ -1,8 +1,14 @@
+build_graph = function( term ) {
+	Bitly.get_story_from_phrases( ['kanye'], function( err, data ) {
+		Bitly.get_story_merged_history( data.story_id, {start: new Date('2013-06-10')}, function( err, history ) {
+			console.log( err, history );
+		});
+	});
+}
+
 Meteor.startup(function() {
 	
-	Meteor.call( 'get_history', 'obama', function( err, history ) {
-		console.log( err, history );
-	});
+	build_graph( 'kanye' );
 	
 	// so you can know if you've successfully in-browser browsed
 	// console.log('Started at ' + location.href);
--- server/router.js
+++ server/router.js
@@ -1,14 +1,11 @@
 if (Meteor.isServer) {
 	Meteor.Router.add({
 		'/get_history/for/:term': function( term ) {
-			
-			ctx = this;
-			
+						
 			Bitly.get_story_from_phrases( [term], function( err, data ) {
 				// console.log( data );
 				ctx.send( 's' );
 				Bitly.get_story_merged_history( data.story_id, {start: new Date('2013-06-10')}, function( err, history ) {
-					return 'bla';
 					return history;
 				});
 				// return story;
@@ -32,13 +29,6 @@ if (Meteor.isServer) {
       Taxonomy.insert(entry);
     }
       
-	});
-	
-	Meteor.startup(function () {
-		var require = __meteor_bootstrap__.require;
-		Future = require('fibers/future');
-	
-		// use Future here
 	});
 
 }
