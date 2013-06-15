012c6bacf76ce2bfae3a1988432a2ce82e6f351a
Morgante Pell <morgante.pell@morgante.net>
2013-06-15 13:47:00 -0400

    modified

--- server/router.js
+++ server/router.js
@@ -1,16 +1,20 @@
 if (Meteor.isServer) {
 	Meteor.Router.add({
-		'/test': function() {
+		'/get_history/for/:term': function( term ) {
 			
-			Bitly.get_story_from_phrases( ['kanye'], function( err, data ) {
+			ctx = this;
+			
+			Bitly.get_story_from_phrases( [term], function( err, data ) {
 				// console.log( data );
+				ctx.send( 's' );
 				Bitly.get_story_merged_history( data.story_id, {start: new Date('2013-06-10')}, function( err, history ) {
-					// console.log( history );
+					return 'bla';
+					return history;
 				});
 				// return story;
 			});
 			
-			return 'going';
+			// return 'going';
 		
 			// return Meteor.settings.bitly.token;
 		},
