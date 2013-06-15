443070126a65d5141313dcba39961157b2ebac38
lingz <lz781@nyu.edu>
2013-06-15 15:24:15 -0400

    sociocast api fixed

---
+++ server/sociocast.js
@@ -0,0 +1,22 @@
+Meteor.methods({
+  sociocast: function(url) {
+    this.unblock();
+    var timestamp = Math.round(+new Date()/1000);
+    var callingUrl = "http://api.sociocast.com/1.0/content/profile?ts=" + 
+      timestamp + "&apikey=" + "g89dv60LlS0QPERttJiI4yht" + "&sig=" + getSig(timestamp) + 
+      "&url=" + url + "&humread=" + "true";
+    var result = Meteor.http.get(callingUrl);
+    console.log(result);
+    if (result.statusCode == 200) {
+      var classification = result.data.classification;
+      var keys = Object.keys(classification);
+      return keys[keys.length - 1];
+    } else if (result.statusCode == 202){
+      // the result hasn't been cached, send it back and wait
+      return null;
+    } else {
+      throw new Meteor.Error(result.statusCode);
+    }
+  }
+});
+
