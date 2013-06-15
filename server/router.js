90f176ba85593534481862c00bf398979ba52a46
Morgante Pell <morgante.pell@morgante.net>
2013-06-15 13:28:43 -0400

    bitly API library

--- server/router.js
+++ server/router.js
@@ -2,9 +2,15 @@ if (Meteor.isServer) {
 	Meteor.Router.add({
 		'/test': function() {
 			
-			Bitly.story_from_phrases( ['clinton'], function( err, story ) {
-				return story;
+			Bitly.get_story_from_phrases( ['kanye'], function( err, data ) {
+				// console.log( data );
+				Bitly.get_story_merged_history( data.story_id, {start: new Date('2013-06-10')}, function( err, history ) {
+					// console.log( history );
+				});
+				// return story;
 			});
+			
+			return 'going';
 		
 			// return Meteor.settings.bitly.token;
 		},
