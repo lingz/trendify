bc91576581e8be6ceb23ffa94b35080a2eb83d5e
lingz <lz781@nyu.edu>
2013-06-15 13:28:08 -0400

    addition api working

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,12 +1,11 @@
 if (Taxonomy.find().count() === 0) {
+  
   //parses one line of the taxonomy files and returns an array of the results
   var parseLine = function(line) {
     var patt = /\[([^\]]+)\]/;
     return patt.exec(line)[1].split(",");
   };
 
-  var tax = new File("taxonomy.txt").open();
-  console.log(tax.readln());
   
 
 }
--- server/router.js
+++ server/router.js
@@ -11,6 +11,16 @@ if (Meteor.isServer) {
 		'/second-test-endpoint': function() {
 			console.log(this.request.body);
 			return 'foo';
-		}
+		},
+    '/load': function() {
+      input = this.request.body;
+      entry = {
+        name: input[input.length-1],
+        father: input.length > 1 ? input[input.length - 2] : null
+      };
+      Taxonomy.insert(entry);
+    }
+      
 	});
+
 }
