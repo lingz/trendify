022f1edbb69d37b95ba1b79bfbc3611fce9d316d
lingz <lz781@nyu.edu>
2013-06-15 16:24:26 -0400

    changed taxonomy feature

--- collections/taxonomy.js
+++ collections/taxonomy.js
@@ -1,7 +1,8 @@
 Taxonomy = new Meteor.Collection('taxonomy');
 
 Meteor.methods({
-  'findSiblings': function(child) {
+  'findSiblings': function(childName) {
+    child = Taxonomy.findOne(name: childName);
     siblings = _.without(Taxonomy.find({father: child.father}).fetch(), child);
 
     return siblings;
